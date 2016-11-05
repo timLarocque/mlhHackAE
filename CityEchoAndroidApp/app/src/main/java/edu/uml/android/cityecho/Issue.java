21509dff25094056c62664f05e8d6e29cf278de0
Adam Gaudreau <adam@Adams-MacBook-Air.local>
2016-11-05 19:25:36 -0400

    Added some query stuff

--- CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/Issue.java
+++ CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/Issue.java
@@ -1,7 +1,38 @@
 package edu.uml.android.cityecho;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by Tim on 11/5/2016.
  */
 public class Issue {
+
+    // Private member variables
+    int mStreetNumber;
+    String mStreetName;
+    String mCity;
+    String mState;
+    String mType;
+    Bitmap mMapImage;
+
+    Issue(int sNum, String sName, String city, String state, String type, Bitmap map) {
+        mStreetNumber = sNum;
+        mStreetName = sName;
+        mCity = city;
+        mState = state;
+        mType = type;
+        mMapImage = map;
+    }
+
+    int getStreetNumber() { return mStreetNumber; }
+    String getStreetName() { return mStreetName; }
+    String getCity() { return mCity; }
+    String getState() { return mState; }
+    String getType() { return mType; }
+    Bitmap getMapImage() { return mMapImage; }
+    String getFullAddress() {
+        return Integer.toString(mStreetNumber) + " "
+                + mStreetName + ", " + mCity + ", " + mState;
+    }
+
 }
