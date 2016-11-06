9eac5da374a233cd0cf197cd815e1966aa0336a3
Adam Gaudreau <adam@Adams-MacBook-Air.local>
2016-11-06 10:59:35 -0500

    Added refresh button.

--- CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/MainActivity.java
+++ CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/MainActivity.java
@@ -8,9 +8,13 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             "http://159.203.136.164/echopy/gcp";
     private IssueAdapter mAdapter;
     private TextView mEmptyStateTextView;
+    private Menu mRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +54,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
+        loadData();
+    }
+
+    public void loadData() {
+        ListView articleListView = (ListView) findViewById(R.id.issueList);
+        ProgressBar pb =  (ProgressBar) findViewById(R.id.loading_indicator);
+        articleListView.setEmptyView(pb);
+        mAdapter = new IssueAdapter(this, new ArrayList<Issue>());
+
+        articleListView.setAdapter(mAdapter);
+
         // Get networking info to determine if we have a connection or not
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -64,6 +80,41 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             loadingIndicator.setVisibility(View.GONE);
 
             mEmptyStateTextView.setText("No internet connection.");
+        }
+        return;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.mRefresh = menu;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.refresh_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuRefresh:
+                LoaderManager loaderManager = getSupportLoaderManager();
+                loaderManager.restartLoader(ARTICLE_LOADER_ID, null, this);
+                loadData();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setRefreshActionButtonState(final boolean refreshing) {
+        if (mRefresh != null) {
+            final MenuItem refreshItem = mRefresh
+                    .findItem(R.id.menuRefresh);
+            if (refreshItem != null) {
+                if (refreshing) {
+                    refreshItem.setActionView(R.layout.actionbar_indeterminite_progress);
+                } else {
+                    refreshItem.setActionView(null);
+                }
+            }
         }
     }
 
@@ -81,8 +132,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         } catch (NullPointerException e) {
             System.out.println("Hmm, couldn't find view.");
         }
-
-        mEmptyStateTextView.setText("Error fetching data.");
 
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
