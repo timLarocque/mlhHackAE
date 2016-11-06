e7df3079f714efa3e81bdcb0b36ba9837a5501cb
timLarocque <timothy_larocque@student.uml.edu>
2016-11-05 21:08:15 -0400

    fixes with spinner adapter

--- CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/IssueActivity.java
+++ CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/IssueActivity.java
@@ -2,6 +2,8 @@ package edu.uml.android.cityecho;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Tim on 11/5/2016.
@@ -11,5 +13,11 @@ public class IssueActivity extends AppCompatActivity {
     public void onCreate(Bundle savedBundleInstance) {
         super.onCreate(savedBundleInstance);
         setContentView(R.layout.activity_issue);
+
+        Spinner spinner = (Spinner) findViewById(R.id.issue_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.issue_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
