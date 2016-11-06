ad31f34d186e278bdb5ac4eb2a24f7cfe7a23f49
timLarocque <timothy_larocque@student.uml.edu>
2016-11-05 20:10:30 -0400

    working on submit form activity

--- CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/IssueActivity.java
+++ CityEchoAndroidApp/app/src/main/java/edu/uml/android/cityecho/IssueActivity.java
@@ -1,7 +1,15 @@
 package edu.uml.android.cityecho;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Tim on 11/5/2016.
  */
-public class IssueActivity {
+public class IssueActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+        setContentView(R.layout.activity_issue);
+    }
 }
